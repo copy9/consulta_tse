33e7ab6f3040d60f4024f9a86c022614501f4c88
Marco Antonio Dutra <109824227+copy9@users.noreply.github.com>
2025-02-28 05:11:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/verificar', async (req, res) => {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36');
 
     console.log('Abrindo o site...');
-    await page.goto('https://www.tse.jus.br/servicos-eleitorais/autoatendimento-eleitoral', { 
+    await page.goto('https://www.tse.jus.br/servicos-eleitorais/autoatendimento-eleitoral#/atendimento-eleitor/onde-votar', { 
       waitUntil: 'networkidle2',
       timeout: 60000 
     });
