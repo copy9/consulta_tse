f197bc87db3edd1539305fd9550bbb1ccca0f9fc
Marco Antonio Dutra <109824227+copy9@users.noreply.github.com>
2025-02-28 04:13:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -76,6 +76,9 @@ app.post('/verificar', async (req, res) => {
     });
     await page.screenshot({ path: 'debug_before_navigation.png' });
 
+    console.log('Esperando os resultados carregarem na tela...');
+    await new Promise(resolve => setTimeout(resolve, 120000));
+
     console.log('Esperando 1 segundo após clicar no botão "Entrar"...');
     await new Promise(resolve => setTimeout(resolve, 1000));
 
